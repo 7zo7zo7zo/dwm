d910aa999773f04a0bbbf9c5f5407e41d755cd5b
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2021-06-29 18:22:42 -0700

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -59,15 +59,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *web[]      = { "firefox", NULL };
-static const char *powermenu[]  = { "/home/steve/scripts/powermenu.sh" };
+static const char *powermenu[]  = { "/home/steve/scripts/powermenu.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = web } },
-	{ MODKEY,                       XK_Down,   spawn,          SHCMD("amixer set Master 2%-") },
-	{ MODKEY,                       XK_Up,     spawn,          SHCMD("amixer set Master 2%+") },
+	{ MODKEY,                       XK_Down,   spawn,          SHCMD("amixer -qM set Master 2%-") },
+	{ MODKEY,                       XK_Up,     spawn,          SHCMD("amixer -qM set Master 2%+") },
 	{ MODKEY,                       XK_Delete, spawn,          {.v = powermenu } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
