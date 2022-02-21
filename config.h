e4e8ba58508e92cb50e518c9c5339b0bcdaaf8a1
7zo7zo7zo <7zo7zo7zo@gmail.com>
2022-02-21 00:18:05 -0800

    fix

--- config.h
+++ config.h
@@ -70,6 +70,10 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = web } },
+	{ 0, XF86XK_AudioRaiseVolume,		   spawn,	   {.v = upvol   } },
+	{ 0, XF86XK_AudioLowerVolume,		   spawn,	   {.v = downvol } },
+	{ 0, XF86XK_AudioMute,			   spawn,	   {.v = mutevol } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
