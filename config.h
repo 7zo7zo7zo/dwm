fb76f0cc8139ad7fce14b205d1685032a5225877
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-08-10 00:05:18 -0700

    Update config.h

--- config.h
+++ config.h
@@ -71,7 +71,7 @@ static const char *medprevcmd[] = { "playerctl", "previous", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	//{ MODKEY,                       XK_w,      spawn,          {.v = web } },
 	{ MODKEY,                       XK_w,      spawn,          SHCMD("$BROWSER") },
